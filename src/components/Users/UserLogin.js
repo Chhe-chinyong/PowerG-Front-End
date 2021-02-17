9e7dbc1aad826f9971044df532c621bcc63a850f
chhechinyong <chhechinyong01@gmail.com>
2021-02-17 23:28:08 +0700

    loginUser to yong

--- src/components/Users/UserLogin.js
+++ src/components/Users/UserLogin.js
@@ -4,7 +4,10 @@ import "antd/dist/antd.css";
 import logo from "../../images/favicon.ico";
 import { Table, Button, Space, Form, Input, InputNumber } from "antd";
 
-const Item = function UserLogin() {
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+
+const { Item } = Form;
+function UserLogin() {
   return (
     <div
       style={{ width: "100%", minHeight: "100vh", backgroundColor: "white" }}
@@ -15,11 +18,66 @@ const Item = function UserLogin() {
         <div className="userLogo">
           <img src={logo} alt="Logo" />
         </div>
-
         {/* Form */}
+
+        <Form
+        //   name="basic"
+        //   initialValues={{
+        //     remember: true,
+        //   }}
+        //   onFinish={onFinish}
+        //   onFinishFailed={onFinishFailed}
+        >
+          {/*Username*/}
+          <Item
+            name={"username"}
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!",
+              },
+            ]}
+          >
+            <Input
+              placeholder="default size"
+              placeholder="Username"
+              prefix={
+                <UserOutlined style={{ color: "gray", opacity: "50%" }} />
+              }
+              className="userInput"
+            />
+          </Item>
+          {/*Password*/}
+          <Item
+            name={"password"}
+            // label="Username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!",
+              },
+            ]}
+          >
+            <Input
+              placeholder="default size"
+              placeholder="Password"
+              prefix={
+                <LockOutlined style={{ color: "gray", opacity: "50%" }} />
+              }
+              className="userInput"
+            />
+          </Item>
+
+          {/* Submit */}
+          <Item className="userSubmit">
+            <Button htmlType="submit" className="userSubmitButton">
+              LOG IN
+            </Button>
+          </Item>
+        </Form>
       </div>
     </div>
   );
-};
+}
 
 export default UserLogin;
